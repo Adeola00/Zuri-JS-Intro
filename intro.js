b418042ff91da829b3804dd66aee01927af8a6ac
Adeola <odusami89@gmail.com>
2022-06-21 15:56:34 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,6 @@
+let name = "Adeola";
+let height = 177;
+let country = "Nigeria";
+
+let result = 'My name is'.concat(" ", name , " and I am ", height, "cm tall. \n","I am from ", country,".");
+document.write(result);
